ef83c477f0ff47125b529df0c3505dee21bb2873
zhaoke <kerncink@gmail.com>
2019-07-25 21:49:17 +0800

    publicPath /website

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extract: true,
   },
 
-  publicPath: process.env.NODE_ENV === 'production' ? '../' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/website' : '/',
   // 修改默认dist为docs，方便github pages查找docs目录下的静态资源
   outputDir: 'docs',
 
