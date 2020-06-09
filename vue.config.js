178e6e50d25b3d815fb8975c645421633461c88e
zhaoke <kerncink@gmail.com>
2020-06-09 11:58:58 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,32 +1,34 @@
+/*
+  pages = {
+    login: {
+      // page 的入口
+      entry: 'src/pages/login/main.js',
+      // 模板来源
+      template: 'public/index1.html',
+      // 在 ${outputDir}/login.html 的输出
+      filename: 'login.html',
+    },
+    index: {
+      entry: 'src/pages/index/main.js',
+      template: 'public/index2.html',
+      filename: 'index.html',
+    },
+    resume: {
+      entry: 'src/pages/resume/main.js',
+      template: 'public/index3.html',
+      filename: 'resume.html',
+    },
+    presentation: {
+      entry: 'src/pages/presentation/main.js',
+      template: 'public/index4.html',
+      filename: 'presentation.html',
+    },
+  },
+*/
+
 const utils = require('./bin/utils');
 const pages = utils.addEntry();
- // pages = {
-//   login: {
-//     // page 的入口
-//     entry: 'src/pages/login/main.js',
-//     // 模板来源
-//     template: 'public/index.html',
-//     // 在 ${outputDir}/login.html 的输出
-//     filename: 'login.html',
-//   },
-//   index: {
-//     entry: 'src/pages/index/main.js',
-//     template: 'public/index.html',
-//     filename: 'index.html',
-//   },
-//   resume: {
-//     entry: 'src/pages/resume/main.js',
-//     template: 'public/index.html',
-//     filename: 'resume.html',
-//   },
-//   presentation: {
-//     entry: 'src/pages/presentation/main.js',
-//     template: 'public/index.html',
-//     filename: 'presentation.html',
-//   },
-// },
-
-
+ 
 module.exports = {
   productionSourceMap: process.env.NODE_ENV !== 'production',
 
