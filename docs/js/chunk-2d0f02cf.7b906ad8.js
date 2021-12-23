6de93d12342008eaecc95e8a520aca81a4633846
zhaoke <zhaoke@xiaomi.com>
2021-12-23 10:05:15 +0800

    refactor：重构vue3+ts

---
+++ docs/js/chunk-2d0f02cf.7b906ad8.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0f02cf"],{"9af7":function(n,c,e){"use strict";e.r(c);var t=e("7a23");const r=Object(t["i"])("h1",null,"秘密页面，禁止访问",-1),u=[r];function a(n,c,e,r,a,o){return Object(t["x"])(),Object(t["h"])("div",null,u)}var o={name:"secret"},s=e("6b0d"),d=e.n(s);const i=d()(o,[["render",a]]);c["default"]=i}}]);
